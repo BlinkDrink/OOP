6a7bfd6af0e64121b791e32e50c19a59c91c4492
BlinkDrink <evrolan1404@gmail.com>
2023-10-20 16:24:41 +0300

    Add the files given by the course instructor and implement the compute cosine function

--- Week3/Problem2/Program.cs
+++ Week3/Problem2/Program.cs
@@ -1,30 +1,38 @@
-﻿using System.Collections.Specialized;
-
-namespace Problem2
+﻿namespace Problem2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter x:");
+            // Prompt user for input
             double x;
+            Console.WriteLine("Enter x:");
+
+            // Read user input
             if (!double.TryParse(Console.ReadLine(), out x))
             {
                 Console.WriteLine("Wrong input.");
                 Console.ReadLine();
                 return;
             }
+
+            // Print the comparison between our implementation of cos and the integrated cos implementation
             Console.WriteLine($"Approximate cos({x}): {ComputeCos(x)}");
             Console.WriteLine($"Accurate cos({x}): {Math.Cos(x)}");
         }
 
+        /// <summary>
+        /// ComputeCos computes the cosine of x using sum approximation
+        /// </summary>
+        /// <param name="x">argument of cos()</param>
+        /// <returns></returns>
         static double ComputeCos(double x)
         {
             // Declaration of vars
             double accuracy = 0.00000001; // accuracy of computation
-            double cosValue = 0;
-            double term = 1.0;
-            int count = 0;
+            double cosValue = 0; // the cosine of x to be returned
+            double term = 1.0; // current member of the sum
+            int count = 0; // keeps count of the index of the current member of the sum
 
             // processing
             do
