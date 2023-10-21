eaf68856e009d399d3e62eade6988e6f4b4ef5e0
BlinkDrink <evrolan1404@gmail.com>
2023-10-21 19:15:27 +0300

    Add method that fills the current hand with cards. Begin implementation of totalHand().

--- Week3/DeckOfCards/DeckOfCards.cs
+++ Week3/DeckOfCards/DeckOfCards.cs
@@ -1,57 +1,123 @@
 ﻿// DeckOfCards.cs
 // DeckOfCards class represents a deck of playing cards.
-using System;
 
 public class DeckOfCards
 {
-   private Card[] deck; // array of Card objects
-   private int currentCard; // index of next Card to be dealt
-   private const int NUMBER_OF_CARDS = 52; // constant number of Cards
-   private Random randomNumbers; // random number generator
-
-   // constructor fills deck of Cards
-   public DeckOfCards()
-   {
-      string[] faces = { "Ace", "Deuce", "Three", "Four", "Five", "Six", 
+    #region Private Fields
+    private Card[] deck; // array of Card objects
+    private int currentCard; // index of next Card to be dealt
+    private const int NUMBER_OF_CARDS = 52; // constant number of Cards
+    private const int CARDS_IN_HAND = 5;
+    private Random randomNumbers; // random number generator
+    private Card[] handOfCards; // current hand of 5 cards 
+    private string[] faces = { "Ace", "Deuce", "Three", "Four", "Five", "Six",
          "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
-      string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+    private string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+    #endregion
 
-      deck = new Card[ NUMBER_OF_CARDS ]; // create array of Card objects
-      currentCard = 0; // set currentCard so deck[ 0 ] is dealt first  
-      randomNumbers = new Random(); // create random number generator
+    #region Constructors
+    // constructor fills deck of Cards
+    public DeckOfCards()
+    {
 
-      // populate deck with Card objects
-      for ( int count = 0; count < deck.Length; count++ )
-         deck[ count ] =
-            new Card( faces[ count % 13 ], suits[ count / 13 ] );
-   } // end DeckOfCards constructor
 
-   // shuffle deck of Cards with one-pass algorithm
-   public void Shuffle()
-   {
-      // after shuffling, dealing should start at deck[ 0 ] again
-      currentCard = 0; // reinitialize currentCard
+        deck = new Card[NUMBER_OF_CARDS]; // create array of Card objects
+        handOfCards = new Card[CARDS_IN_HAND]; // create array of Card objects representing the hand
+        currentCard = 0; // set currentCard so deck[ 0 ] is dealt first  
+        randomNumbers = new Random(); // create random number generator
 
-      // for each Card, pick another random Card and swap them
-      for ( int first = 0; first < deck.Length; first++ )
-      {
-         // select a random number between 0 and 51 
-         int second = randomNumbers.Next( NUMBER_OF_CARDS );
+        // populate deck with Card objects
+        for (int count = 0; count < deck.Length; count++)
+            deck[count] =
+               new Card(faces[count % 13], suits[count / 13]);
+    }
+    #endregion
 
-         // swap current Card with randomly selected Card
-         Card temp = deck[ first ];
-         deck[ first ] = deck[ second ];
-         deck[ second ] = temp;
-      } // end for
-   } // end method Shuffle
+    #region Private Methods
+    /// <summary>
+    /// Recommended method used to store how many of each of the cards
+    /// we have in our hand. (i.e. a hand "Ace Spades", "Jack Hearts", "Jack Spades", "Two Diamonds", "Ace Diamonds" will
+    /// look like [1,0,0,0,0,0,0,0,0,2,0,0,2])
+    /// </summary>
+    /// <returns></returns>
+    private int[] totalHand()
+    {
+        int[] countPerFace = new int[13];
 
-   // deal one Card
-   public Card? DealCard()
-   {
-      // determine whether Cards remain to be dealt
-      if ( currentCard < deck.Length )
-         return deck[ currentCard++ ]; // return current Card in array
-      else
-         return null; // indicate that all Cards were dealt
-   } // end method DealCard
-} // end class DeckOfCards
+        foreach (Card card in handOfCards)
+        {
+            int cardIndex;
+        }
+
+        return countPerFace;
+    }
+    #endregion
+
+    #region Public Methods
+    // shuffle deck of Cards with one-pass algorithm
+    public void Shuffle()
+    {
+        // after shuffling, dealing should start at deck[ 0 ] again
+        currentCard = 0; // reinitialize currentCard
+
+        // for each Card, pick another random Card and swap them
+        for (int first = 0; first < deck.Length; first++)
+        {
+            // select a random number between 0 and 51 
+            int second = randomNumbers.Next(NUMBER_OF_CARDS);
+
+            // swap current Card with randomly selected Card
+            Card temp = deck[first];
+            deck[first] = deck[second];
+            deck[second] = temp;
+        }
+    }
+
+    // deal one Card
+    public Card? DealCard()
+    {
+        // determine whether Cards remain to be dealt
+        if (currentCard < deck.Length)
+            return deck[currentCard++]; // return current Card in array
+        else
+            return null; // indicate that all Cards were dealt
+    }
+
+    /// <summary>
+    /// Fills the current hand with cards by drawing the cards from the deck
+    /// If the deck has no more cards, a message gets displayed which prompts 
+    /// the user to reshuffle the deck and start again 
+    /// </summary>
+    public void FillHand()
+    {
+        // Check if we have enough cards, if not the ask to reshuffle the deck
+        if (currentCard >= deck.Length - 5)
+        {
+            Console.WriteLine("Not enough cards to fill the hand anew. Do you want to reshuffle (y/n)?");
+            string? answer = Console.ReadLine();
+            if (answer == "y") { Shuffle(); }
+        }
+
+        // Draw 5 cards to fill the hand
+        for (int i = 0; i < CARDS_IN_HAND; i++)
+            handOfCards[i] = DealCard()!;
+    }
+
+    /// <summary>
+    /// Draw 5 cards to fill the current hand
+    /// </summary>
+    /// <param name="face"></param>
+    /// <returns>True if there are two cards of the same face, false otherwise</returns>
+    public bool TwoSameFacesInHand(string face)
+    {
+
+
+        if (currentCard >= deck.Length)
+        {
+            Console.WriteLine("All cards have been dealt! Shuffle the deck to begin anew.");
+            return false;
+        }
+        return false;
+    }
+    #endregion
+}
--- Week3/DeckOfCards/DeckOfCardsTest.cs
+++ Week3/DeckOfCards/DeckOfCardsTest.cs
@@ -1,23 +1,22 @@
 ﻿// DeckOfCardsTest.cs
 // Card shuffling and dealing application.
-using System;
 
 public class DeckOfCardsTest
 {
-   // execute application
-   public static void Main( string[] args )
-   {
-      DeckOfCards myDeckOfCards = new DeckOfCards();
-      myDeckOfCards.Shuffle(); // place Cards in random order
+    // execute application
+    public static void Main(string[] args)
+    {
+        DeckOfCards myDeckOfCards = new DeckOfCards();
+        myDeckOfCards.Shuffle(); // place Cards in random order
 
-      // display all 52 Cards in the order in which they are dealt
-      for ( int i = 0; i < 52; i++ )
-      {
-              Console.Write( "{0,-19}", myDeckOfCards.DealCard() );
+        // display all 52 Cards in the order in which they are dealt
+        for (int i = 0; i < 52; i++)
+        {
+            Console.Write("{0,-19}", myDeckOfCards.DealCard());
 
-         if ( ( i + 1 ) % 4 == 0 )
-            Console.WriteLine();
-      } // end for
-   } // end Main
-} // end class DeckOfCardsTest
+            if ((i + 1) % 4 == 0)
+                Console.WriteLine();
+        }
+    }
+}
 
