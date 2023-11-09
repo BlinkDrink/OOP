916d6ee29443dca6ad1a8996b05711643da40319
BlinkDrink <evrolan1404@gmail.com>
2023-11-09 19:32:16 +0200

    Initialize static list products. Implement ToString Method of Product and initializing constructor

--- Homework4/Homework4/Homework4/Product.cs
+++ Homework4/Homework4/Homework4/Product.cs
@@ -2,7 +2,7 @@
 {
     public enum YearlyQuarter
     {
-        First,
+        First = 1,
         Second,
         Third,
         Fourth
@@ -10,8 +10,8 @@
 
     public enum Type
     {
-        M,
-        F
+        M = 'M',
+        F = 'F'
     }
 
     public class Product
@@ -19,13 +19,37 @@
         #region Properties
         private long cnt;
         private string ID;
-        private Random rnd;
+        private static Random rnd;
         private List<int> WeeklyPurchases;
 
         public Type Category { get; init; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public YearlyQuarter Quarter { get; set; }
         #endregion
 
+        #region Constructors
+        /// <summary>
+        /// General purpose constructor for initializing description, category, weeklyPurchases and price
+        /// </summary>
+        /// <param name="description">Initializer of Description</param>
+        /// <param name="category">Initializer of Category</param>
+        /// <param name="weeklyPurchases">Initializer of WeeklyPurchases</param>
+        /// <param name="price">Initializer of Price</param>
+        public Product(string description, Type category, List<int> weeklyPurchases, decimal price)
+        {
+            Quarter = (YearlyQuarter)(rnd.Next(3) + 1);
+            Description = description;
+            Category = category;
+            WeeklyPurchases = weeklyPurchases;
+            Price = price;
+            ID = $"{Category}{rnd.Next(100001, 999999)}";
+        }
+        #endregion    
 
-
+        public override string ToString()
+        {
+            return $"{Description} {Category} {WeeklyPurchases} {Price}";
+        }
     }
 }
--- Homework4/Homework4/Homework4/Program.cs
+++ Homework4/Homework4/Homework4/Program.cs
@@ -2,6 +2,22 @@
 {
     internal class Program
     {
+        static List<Product> products = new List<Product> {
+        new Product("Electric sander", Type.M, new List<int> { 99, 82, 81, 79 },  157.98m),
+        new Product ("Power saw", Type.M, new List < int > { 99, 86, 90, 94 }, 157.98m) ,
+        new Product ( "Sledge hammer", Type.F, new List<int> { 93, 92, 80, 87 },  21.50m ),
+        new Product ( "Hammer", Type.M, new List < int > { 97, 89, 85, 82 }, 11.99m ),
+        new Product ( "Lawn mower", Type.F, new List < int > { 35, 72, 91, 70 }, 139.50m ),
+        new Product ( "Screwdriver", Type.F, new List < int > { 88, 94, 65, 91 }, 56.99m ),
+        new Product ( "Jig saw", Type.M, new List < int > { 75, 84, 91, 39 }, 11.00m ),
+        new Product ( "Wrench", Type.F, new List < int > { 97, 92, 81, 60 }, 17.50m ),
+        new Product ( "Sledge hammer", Type.M, new List < int > { 75, 84, 91, 39 }, 21.50m ),
+        new Product ( "Hammer", Type.F, new List < int > { 94, 92, 91, 91 }, 11.99m ),
+        new Product ( "Lawn mower", Type.M, new List < int > { 96, 85, 91, 60 }, 179.50m ),
+        new Product ( "Screwdriver", Type.M, new List < int > { 96, 85, 51, 30 }, 66.99m ),
+        };
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("");
