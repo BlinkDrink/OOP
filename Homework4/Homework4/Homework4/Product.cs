d36b814848d5b11724b1063f00340364f5b8b4a4
BlinkDrink <evrolan1404@gmail.com>
2023-11-09 18:56:28 +0200

    Initialize Homework4 and begin implementing Product Class

---
+++ Homework4/Homework4/Homework4/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace Homework4
+{
+    public enum YearlyQuarter
+    {
+        First,
+        Second,
+        Third,
+        Fourth
+    }
+
+    public enum Type
+    {
+        M,
+        F
+    }
+
+    public class Product
+    {
+        #region Properties
+        private long cnt;
+        private string ID;
+        private Random rnd;
+        private List<int> WeeklyPurchases;
+
+        public Type Category { get; init; }
+        #endregion
+
+
+
+    }
+}
