a2f8abe5aa435eaa2a33b5aac9df66f613ef6e3b
BlinkDrink <evrolan1404@gmail.com>
2023-11-21 17:20:57 +0200

    Implement interface TextBox

---
+++ Homework5/Homework5/Homework5/Interfaces/TextBox.cs
@@ -0,0 +1,29 @@
+﻿namespace Homework5.Interfaces
+{
+    public abstract class TextBox
+    {
+        protected string text;
+        protected string baseText;
+
+        public string Text { get => text; private set { text = value; } }
+        public string BaseText { get => baseText; private set { baseText = value; } }
+
+        #region Constructors
+        public TextBox()
+        {
+            Text = $"{GetType()}:Type text";
+            BaseText = $"{GetType()}:Type baseText";
+        }
+        #endregion
+
+        #region Methods
+
+        protected void TypeText() { Console.WriteLine(text); }
+
+        public abstract void EditTextAllowed();
+
+        public abstract void EditTextDisAllowed();
+
+        #endregion
+    }
+}
