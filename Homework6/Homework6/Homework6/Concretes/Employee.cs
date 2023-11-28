a021e89d48016fe014e4d909fb9e318be734fd4f
BlinkDrink <evrolan1404@gmail.com>
2023-11-28 23:58:44 +0200

    Add Employee and Manager classes

---
+++ Homework6/Homework6/Homework6/Concretes/Employee.cs
@@ -0,0 +1,35 @@
+﻿namespace Homework6.Concretes
+{
+    public class Employee
+    {
+        #region Properties
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string worksAt;
+
+        public string WorksAt
+        {
+            get { return worksAt; }
+            set { worksAt = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public Employee(string name, string worksAt)
+        {
+            Name = name;
+            WorksAt = worksAt;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString() { return $"{Name} {WorksAt}"; }
+        #endregion
+    }
+}
---
+++ Homework6/Homework6/Homework6/Concretes/Manager.cs
@@ -0,0 +1,11 @@
+﻿namespace Homework6.Concretes
+{
+    public class Manager : Employee
+    {
+
+        public Manager(string name, string worksAt) : base(name, worksAt) { }
+
+
+
+    }
+}
