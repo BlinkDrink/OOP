b3e64a6039ec76e3a39840023e37af453af3db23
BlinkDrink <evrolan1404@gmail.com>
2023-12-31 15:53:18 +0200

    Add class extension with key word this

---
+++ Midterm/Preparation/InvoiceProblem/InvoiceProblem/InvoiceExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace InvoiceProblem
+{
+    public static class InvoiceExtensions
+    {
+        public static void AddAllInvoice(this Invoice invoice, List<InvoiceDetail> details)
+        {
+            invoice.InvoiceItems.AddRange(details);
+        }
+    }
+}
