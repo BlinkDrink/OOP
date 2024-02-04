fee295ec31781d2e9abee351d588ce0c516dac8a
BlinkDrink <evrolan1404@gmail.com>
2024-02-04 13:56:41 +0200

    Add delegate of type OrderHandler that will handle the order event

---
+++ FinalExam/TradeServices/TradeServices/OrderEventArgs.cs
@@ -0,0 +1,25 @@
+﻿namespace TradeServices.DelegatesAndEventArgs
+{
+    /// <summary>
+    /// Provides data for the Order event.
+    /// </summary>
+    public class OrderEventArgs : EventArgs
+    {
+        public string ProductCode { get; private set; }
+        public int Quantity { get; private set; }
+
+        public OrderEventArgs(string productCode, int quantity)
+        {
+            ProductCode = productCode;
+            Quantity = quantity;
+        }
+    }
+}
+
+namespace TradeServices.DelegatesAndEventArgs
+{
+    /// <summary>
+    /// Represents the method that will handle the Order event.
+    /// </summary>
+    public delegate void OrderHandler(object sender, OrderEventArgs e);
+}
--- FinalExam/TradeServices/TradeServices/OrderProduct.xaml.cs
+++ FinalExam/TradeServices/TradeServices/OrderProduct.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using TradeServices.DelegatesAndEventArgs;
 
 namespace TradeServices.UserControls
 {
     public partial class OrderProduct : UserControl
     {
+        // Event declaration using the defined delegate
+        public event OrderHandler OrderPlaced;
+
         public OrderProduct()
         {
             InitializeComponent();
@@ -15,6 +19,13 @@ namespace TradeServices.UserControls
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
             txtQty.Text = "0";
+        }
+
+        private void Order_Click(object sender, RoutedEventArgs e)
+        {
+            var productCode = ProductComboBox.SelectedItem.ToString(); // Assuming the ComboBox is populated with product codes
+            var quantity = int.TryParse(txtQty.Text, out int qty) ? qty : 0; // Safely parse the quantity
+            OrderPlaced?.Invoke(this, new OrderEventArgs(productCode, quantity)); // Raise the OrderPlaced event
         }
     }
 }
